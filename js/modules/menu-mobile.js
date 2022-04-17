a200bee9581f777b1b9cf3efe24f87fcee718c8c
Giovana Sanches <giovanaclemente2139@gmail.com>
2022-04-17 00:14:50 -0300

    menu-mobile

---
+++ js/modules/menu-mobile.js
@@ -0,0 +1,21 @@
+import outsideClick from "./outsideclick.js";
+
+export default function initMenuMobile() {
+  const menuButton = document.querySelector('[data-menu="Button"]');
+  const menuList = document.querySelector('[data-menu="List"]');
+  const eventos = ["click", "touchstart"];
+
+  if (menuButton) {
+    function openMenu(event) {
+      menuList.classList.add("active");
+      menuButton.classList.add("active");
+
+      outsideClick(menuList, eventos, () => {
+        menuList.classList.remove("active");
+        menuButton.classList.remove("active");
+      });
+    }
+  }
+
+  eventos.forEach((evento) => menuButton.addEventListener(evento, openMenu));
+}
