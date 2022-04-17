6b72ca682df9aad7da323b37530f14eddcfb4628
Giovana Sanches <giovanaclemente2139@gmail.com>
2022-04-16 23:02:29 -0300

    outsideclick

---
+++ js/modules/outsideclick.js
@@ -0,0 +1,21 @@
+export default function outsideClick(element, events, callback) {
+  const html = document.documentElement;
+  const outside = "data-outside";
+
+  if (!element.hasAttribute(outside)) {
+    events.forEach((userEvent) => {
+      html.addEventListener(userEvent, handleOutsideClick);
+    });
+    element.setAtrrubute(outside, "");
+  }
+
+  function handleOutsideClick(event) {
+    if (!element.contains(event.target)) {
+      element.removeAtrribute(outside);
+      events.forEach((userEvent) => {
+        html.removeEventListener("click", handleOutsideClick);
+      });
+      callback();
+    }
+  }
+}
